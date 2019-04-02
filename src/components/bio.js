91ab8a548d0cd2905def1c7c7ed754bee1f242f6
Saravana <saravanakumar@nfnlabs.in>
2019-04-02 11:47:34 +0530

    Bio image dimension changed

--- src/components/bio.js
+++ src/components/bio.js
@@ -30,7 +30,7 @@ function Bio() {
               style={{
                 marginRight: rhythm(1 / 2),
                 marginBottom: 0,
-                minWidth: 150,
+                minWidth: 100,
                 borderRadius: `100%`,
               }}
               imgStyle={{
@@ -60,7 +60,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 150, height: 150) {
+        fixed(width: 100, height: 100) {
           ...GatsbyImageSharpFixed
         }
       }
