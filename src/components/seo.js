0e901b672eade7e2b41e809c5f3c4620dbd328bd
Saravana <saravanakumar@nfnlabs.in>
2019-04-03 12:09:14 +0530

    SEO.js altered

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,91 +9,143 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, keywords, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            
-            author
-          }
-        }
+
+const SEOO = ({title,description}) => (
+  <StaticQuery
+  query={queryy}
+  render={({
+    site: {
+      siteMetadata : {
+        title,
+        description,
       }
-    `
+    }
+  }) => {
+    const seoo = {
+      title: title || defaultTitle,
+      description: description || defaultDescription,
+    };
+    return (
+      <>
+      <Helmet title={seoo.title}>
+      <meta name="description" content={seoo.description}/>
+      </Helmet>
+      )
+  }}
+  />
   )
 
-  // const metaDescription = description || site.siteMetadata.description
+export default SEOO
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      // title={title}
-      // titleTemplate={`%s | ${site.siteMetadata.title}`}
-      // meta={[
-      //   {
-      //     name: `description`,
-      //     content: metaDescription,
-      //   },
-      //   {
-      //     property: `og:title`,
-      //     content: title,
-      //   },
-      //   {
-      //     property: `og:description`,
-      //     content: metaDescription,
-      //   },
-      //   {
-      //     property: `og:type`,
-      //     content: `website`,
-      //   },
-      //   {
-      //     name: `twitter:card`,
-      //     content: `summary`,
-      //   },
-      //   {
-      //     name: `twitter:creator`,
-      //     content: site.siteMetadata.author,
-      //   },
-      //   {
-      //     name: `twitter:title`,
-      //     content: title,
-      //   },
-      //   {
-      //     name: `twitter:description`,
-      //     content: metaDescription,
-      //   },
-      // ]
-      //   .concat(
-      //     keywords.length > 0
-      //       ? {
-      //           name: `keywords`,
-      //           content: keywords.join(`, `),
-      //         }
-      //       : []
-      //   )
-      //   .concat(meta)}
-    />
-  )
-}
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  keywords: [],
-  description: ``,
-}
-
-SEO.propTypes = {
+SEOO.propTypes = {
+  title: PropTypes.string,
   description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
 }
 
-export default SEO
+SEOO.defaultProps = {
+  title: null,
+  defaultProps: null,
+}
+
+const queryy =  graphql`
+query SEOO {
+  site {
+    siteMetadata {
+      title
+      description
+    }
+  }
+}
+`;
+
+
+// function SEO({ description, lang, meta, keywords, title }) {
+//   const { site } = useStaticQuery(
+//     graphql`
+//       query {
+//         site {
+//           siteMetadata {
+//             title
+//             description
+//             author
+//           }
+//         }
+//       }
+//     `
+//   )
+
+//   const metaDescription = description || site.siteMetadata.description
+
+//   return (
+//     <Helmet
+//       htmlAttributes={{
+//         lang,
+//       }}
+//       title={title}
+//       titleTemplate={`%s | ${site.siteMetadata.title}`}
+//       meta={[
+//         {
+//           name: `description`,
+//           content: metaDescription,
+//         },
+//         {
+//           property: `og:title`,
+//           content: title,
+//         },
+//         {
+//           property: `og:description`,
+//           content: metaDescription,
+//         },
+//         {
+//           property: `og:type`,
+//           content: `website`,
+//         },
+//         {
+//           name: `twitter:card`,
+//           content: `summary`,
+//         },
+//         {
+//           name: `twitter:creator`,
+//           content: site.siteMetadata.author,
+//         },
+//         {
+//           name: `twitter:title`,
+//           content: title,
+//         },
+//         {
+//           name: `twitter:description`,
+//           content: metaDescription,
+//         },
+//       ]
+//         .concat(
+//           keywords.length > 0
+//             ? {
+//                 name: `keywords`,
+//                 content: keywords.join(`, `),
+//               }
+//             : []
+//         )
+//         .concat(meta)}
+//     />
+//   )
+// }
+
+// SEO.defaultProps = {
+//   lang: `en`,
+//   meta: [],
+//   keywords: [],
+//   description: ``,
+// }
+
+// SEO.propTypes = {
+//   description: PropTypes.string,
+//   lang: PropTypes.string,
+//   meta: PropTypes.arrayOf(PropTypes.object),
+//   keywords: PropTypes.arrayOf(PropTypes.string),
+//   title: PropTypes.string.isRequired,
+// }
+
+// export default SEO
